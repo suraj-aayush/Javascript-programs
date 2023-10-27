4a7648d760f204b9bcab2aafb67aab2af05eee1c
Aayush Kumar <105626112+suraj-aayush@users.noreply.github.com>
2023-10-27 03:53:08 +0000

    array 2 completed

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -6,4 +6,28 @@ const dc = ["flash", "superman", "superwoman"]
 // console.log(marvel[3][1]);
 
 const newHeroes = marvel.concat(dc)
-console.log(newHeroes)
+console.log(newHeroes)  // concatinates all array
+
+// SPREAD OPERATOR
+console.log(...marvel, ...dc ,'\n');  // concatinates any number of arrays ...array_name
+
+const ajeebArray = [ 1,2,3,4, [3,5,6,6], [7,8,3,6,[89, 99 ,65 ]] ]
+console.log(ajeebArray)
+
+const goodArray = ajeebArray.flat(Infinity)
+console.log(`\n our good flat array is ${goodArray}\n`);
+
+console.log(Array.isArray("Hello bro"));
+console.log(Array.from("hello_bro"));
+
+console.log(Array.from({name : "aayush"}));   // key value pair.. we need to specify to make array of keys / values 
+
+const n1=100
+const n2=200
+const n3=300
+const n4=400
+console.log('new array of numbers is')
+console.log(Array.of(n1,n2,n3,n4));
+
+
+// read more about .. isArray... from.... of
