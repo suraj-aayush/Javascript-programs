8ecbe6dd8a9e1720dba324af5903294892270eff
Aayush Kumar <105626112+suraj-aayush@users.noreply.github.com>
2023-11-14 16:30:07 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,30 @@
+const fun1 = function(num)
+{
+    return num*5
+}
+console.log(fun1(3));
+//----------------------------------------------=------------------------------
+// DECLARING AND CALLING FUNCTION AT THE SAME TIME
+// IIFE -> IMMEDIATELY INVOKED FUNCTION EXPRESSIONS
+
+
+// first pair of brackets shows that the function is wrapped 
+// last pair of brackets show that the function is invoked immedaitely
+
+// this is NAMED IIFE
+(function chai()
+{
+    console.log("aayush 2.0");
+})();
+
+//-----------------------------------=-------------------
+// ARROW FUNCTION
+
+( ()=>{
+    console.log("Arrow function ");
+}) ()
+
+// WITH ARGUMENT
+( (name)=>{
+    console.log(` ARROW FUNCTION WITH NAME AS ${name} `);
+}) ('aayush3.0')
