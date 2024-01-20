886fb999d461144d1e588ef821998f8656ee73c6
Aayush Kumar <105626112+suraj-aayush@users.noreply.github.com>
2024-01-20 21:41:23 +0530

    h2 added
    basic h2 added

--- 01_basics/dataType.js
+++ 01_basics/dataType.js
@@ -1,53 +1,27 @@
-/* 
-7 types
-
-String
-Number
-Boolean
-null
-undefined
-Symbol
-BigInt
-*/
-
-const marks = 92;
-const decimal = 2.4;
-
-const isLoggedIn = false;
-const outsideTemp = null;
-let userEmail; // undefined automatically
-
-
-const id = Symbol('1231');
-const uid = Symbol("1231");
-
-console.log(id == uid );
-
-const biginteger = 1231231231231231
-
-// REFERENCE TYPE ( NON PRIMITIVE )
 /*
-    array -> typeof(array) = object
-    objects -> typeof(objects ) = object
-    functions  -> datatype--> typeof(function) = object function
+         DATATYPE
+
+null
+bigint
+string
+let
+var
+boolean
+undefined
+symbol -> unique
+null -> object
+
 */
 
-const subjects = ["eng","hindi","maths","phy"];
+let name="ak"
+let lname="kumar"   // string
+contact = 909090    // number
+var roll=2          // VARIABLES
+const id = 210211  // ALWAYS FIXED VALUE
 
-//objects->
-let myObj = {
-    name:"aam",
-    roll:2,
-    sec:'B'
-}
+console.log(typeof lname);
+console.log(typeof contact);
 
-console.log(" type of of myObj is ", typeof(myObj))
-
-// functions
-
-const myFunc = function () {
-    console.log("hiii");
-    
-}
-// calling the function
-myFunc();
+// type of undefined and null
+console.log(typeof null)
+console.log(typeof undefined)
