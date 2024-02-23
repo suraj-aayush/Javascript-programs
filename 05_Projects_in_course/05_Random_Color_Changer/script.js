34336ac3f81a691365f981246f6c2372691dd6fb
Aayush Kumar <aayushagrawal296@gmail.com>
2024-02-23 17:30:23 +0530

    project

--- 05_Projects_in_course/05_Random_Color_Changer/script.js
+++ 05_Projects_in_course/05_Random_Color_Changer/script.js
@@ -12,11 +12,23 @@
     const start = document.getElementById('start');
     const stop = document.getElementById('stop');
 
-const colorize = function(){
-    setInterval()
+    function changeBg(){
     document.body.style.backgroundColor=randomColor();
+
+    }
+
+let intervalId;    
+const colorize = function(){
+    if(!intervalId)
+     intervalId = setInterval(changeBg, 1000);
 }
 
-    let intervalId;
+const decolor = function(){
+    clearInterval(intervalId);
+    intervalId = null;
+}
 
-    start.addEventListener('click', colorize)
+    
+
+    start.addEventListener('click', colorize);
+    stop.addEventListener('click',decolor);
