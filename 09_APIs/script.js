f9ebd9ddf82a70a693091ffddbd67ee801542eb2
Aayush Kumar <aayushagrawal296@gmail.com>
2024-02-25 22:58:44 +0530

    continue

--- 09_APIs/script.js
+++ 09_APIs/script.js
@@ -2,5 +2,8 @@
 const url = 'https://api.github.com/users/suraj-aayush'
 const xhr = new XMLHttpRequest();
 xhr.open('GET',url);
-console.log(xhr.readyState);
+xhr.onreadystatechange = function(){
+    console.log(xhr.readyState);
+}
+// console.log(xhr.readyState);
 xhr.send();
